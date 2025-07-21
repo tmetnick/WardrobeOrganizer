d68f69dd850623b3ab8f980a4edd14aaa6dd22b6
Teagan Metnick <teaganmetnick@Teagans-MBP.lan>
2025-07-20 20:37:58 -0400

    Fix case in User model import for analytics route

--- server/routes/analytics.js
+++ server/routes/analytics.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('../models/user');
 const ClothingItem = require('../models/ClothingItem');
 
 router.get('/user-stats', async (req, res) => {
